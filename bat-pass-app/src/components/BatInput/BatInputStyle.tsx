80e239dd73038ccbd62458f0c43f177d12a74cbd
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-03-12 20:46:21 -0300

    etilizando texto

--- bat-pass-app/src/components/BatInput/BatInputStyle.tsx
+++ bat-pass-app/src/components/BatInput/BatInputStyle.tsx
@@ -3,11 +3,13 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     width:345,
-    backgroundColor: '#yellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
   input:{
+    backgroundColor:'yellow',
+    fontWeight:'bold',
+    fontSize:25,
     width:345,
     height: 40,
     padding: 10,  
